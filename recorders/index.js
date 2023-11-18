a8f1abbbfffd89bdac0659f7f1f9975f5e9ffc3c
Navid <navidshad72@gmail.com>
2023-11-18 22:19:19 +0300

    read all recorders before load metho to prevent module loading issue on build systems like webpack.

--- recorders/index.js
+++ recorders/index.js
@@ -1,9 +1,16 @@
 const path = require('path')
 
+const recorders = {
+  arecord : require('./arecord'),
+  rec : require('./rec'),
+  sox : require('./sox')
+}
+
 function load (recorderName) {
   try {
-    const recoderPath = path.resolve(__dirname, recorderName)
-    return require(recoderPath)
+    if(recorders[recorderName])
+      return recorders[recorderName]
+    else throw 1;
   } catch (err) {
     if (err.code === 'MODULE_NOT_FOUND') {
       throw new Error(`No such recorder found: ${recorderName}`)
